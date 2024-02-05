e96d88320b873d54592d09dceae9abefe6c4b6b0
George fe <georgefen13@gmail.com>
2024-02-05 16:25:37 +0800

    minor update

--- IQAsoftware/settings.py
+++ IQAsoftware/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qnk*lto6by)^@&i-=8ryk4q2-(*u*d8w&9)vj$p(cyby!a7v6w'
+SECRET_KEY = '********'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
